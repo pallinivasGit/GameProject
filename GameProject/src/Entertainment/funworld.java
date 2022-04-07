d3845f769f5814b203d7c7347abfe26774d96a19
skila <skila@192.168.1.137>
2022-04-06 23:38:21 -0400

    My first commit

---
+++ GameProject/src/Entertainment/funworld.java
@@ -0,0 +1,14 @@
+package Entertainment;
+
+public class funworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to the game world");
+		System.out.println("Ujwal entertainment and game world");
+		
+
+	}
+
+}
